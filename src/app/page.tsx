85ea8ddb91c8cd0e26ea327aab4cc3705e0a23f0
fernandocst03 <fernando.dorantescst@gmail.com>
2023-09-25 19:04:45 -0600

    add page index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,145 @@
+'use client'
+
 import Image from 'next/image'
+import { Todos } from './components/Todos'
+import { useState } from 'react'
+import { type TodoTitle, type FilterValue, type TodoId, type Todo as TodoType, type TodoComments } from './types'
+import { TODO_FILTERS } from './consts'
+import { Footer } from './components/Footer'
+import { Filters } from './components/Filters'
+import { CreateTodo } from './components/CreateTodo'
 
-export default function Home() {
+const mockTodos = [
+  {
+    id: '1',
+    title: 'Todo 1',
+    comments: 'comentarios',
+    date: '16/09/2023',
+    completed: false
+  },
+  {
+    id: '2',
+    title: 'Todo 2',
+    comments: 'comentarios',
+    date: '16/09/2023',
+    completed: true
+  },
+  {
+    id: '3',
+    title: 'Todo 3',
+    comments: 'comentarios',
+    date: '16/09/2023',
+    completed: false
+  },
+  {
+    id: '4',
+    title: 'Todo 4',
+    comments: 'comentarios',
+    date: '16/09/2023',
+    completed: true
+  },
+  {
+    id: '5',
+    title: 'Todo 5',
+    comments: 'comentarios',
+    date: '16/09/2023',
+    completed: true
+  }
+]
+
+export default function Home(): JSX.Element {
+  const [todos, setTodos] = useState(mockTodos)
+  const [loading, setLoading] = useState(false)
+  const [filterSelected, setfilterSelected] = useState<FilterValue>(TODO_FILTERS.ALL)
+
+  const handleRemove = ({ id }: TodoId): void => { // recibe id de tipo TodoId y no regresa nada la funcion
+    const newTodos = todos.filter(todo => todo.id !== id) // guarda los id que sean diferentes al id que pasamos
+    setTodos(newTodos)
+  }
+
+  const handleCompleted = ({ id, completed }: Pick<TodoType, 'id' | 'completed'>): void => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed
+        }
+      }
+
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+  const handleRemoveAllCompleted = (): void => {
+    setLoading(true)
+    setTimeout(() => {
+      const newTodos = todos.filter(todo => !todo.completed)
+      setTodos(newTodos)
+      setLoading(false)
+    }, 1200)
+  }
+
+  const handleFilterChange = (filter: FilterValue): void => {
+    setfilterSelected(filter)
+  }
+
+  const filteredTodos = todos.filter(todo => {
+    if (filterSelected === TODO_FILTERS.ACTIVE) return !todo.completed
+    if (filterSelected === TODO_FILTERS.COMPLETED) return todo.completed
+    return todo
+  })
+
+  function getDate(): string {
+    const hoy = new Date()
+    const dia = hoy.getDate().toString().padStart(2, '0')
+    const mes = (hoy.getMonth() + 1).toString().padStart(2, '0') // Sumamos 1 porque los meses van de 0 a 11
+    const anio = hoy.getFullYear()
+
+    return `${dia}/${mes}/${anio}`
+  }
+
+  const handleAddTodo = ({ title }: TodoTitle, { comments }: TodoComments): void => {
+    const newTodo = {
+      title,
+      id: crypto.randomUUID(),
+      comments,
+      date: getDate(),
+      completed: false
+    }
+
+    console.log(new Date())
+
+    const newTodos = [...todos, newTodo]
+    setTodos(newTodos)
+  }
+
+  const activeCount = todos.filter(todo => !todo.completed).length
+  const completedCount = todos.filter(todo => todo.completed).length
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex flex-col w-full min-h-screen py-20 items-center">
+      <Image
+        className="relative w-1/3 h-auto"
+        src="/todoImage.svg"
+        alt="Next.js Logo"
+        width={100}
+        height={37}
+        priority
+      />
+      <Filters filterSelected={filterSelected} onFilterChange={handleFilterChange} />
+      <Todos
+        todos={filteredTodos}
+        onToggleCompleteTodo={handleCompleted}
+        onRemoveTodo={handleRemove}
+      />
+      <Footer
+        activeCount={activeCount}
+        completedCount={completedCount}
+        onClearCompleted={handleRemoveAllCompleted}
+        loading={loading}
+      />
+      <CreateTodo onAddTodo={handleAddTodo} />
     </main>
   )
 }
